1fd90ca45d48c0072604dfd37ecc92d0241a04cd
johnonly10 <johnortigas12@gmail.com>
2026-01-04 16:50:12 +0800

    feat: ministry module with updated component

--- resources/views/staff/ministry/edit.blade.php
+++ resources/views/staff/ministry/edit.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
     <div class="containerr-fluid p-0">
-        <x-page-title title="Create New Departments" active="Create New Department" home="Departments" :home-route="route('staff.departments.index')" />
+        <x-page-title title="Update Ministry" active="Update  Ministry" home="Ministry" :home-route="route('staff.ministries.index')" />
 
         <div class="row">
             <div class="col-lg-12">
                 <div class="white_card card_height_100 mb_30">
-                    <x-white-card-header title="Department's Information"
-                        subTitle="Fill in the details to fill the Department information" />
+                    <x-white-card-header title="Ministry's Information"
+                        subTitle="Update in the details to the Ministry information" />
 
                     <div class="white_card_body">
                         <div class="card-body pt-4">
-                            <form action="{{ route('staff.departments.store') }}" method="POST">
+                            <form action="{{ route('staff.ministries.update', $ministry->id) }}" method="POST">
                                 @csrf
+                                @method('PUT')
                                 <div class="mb-4">
                                     <h5 class="mb-3 text-primary">
-                                        <i class="fas fa-building me-2"></i>
-                                        Department Information
+                                        <i class="fas fa-church me-2"></i>
+                                        Ministry Information
                                     </h5>
                                     <div class="row g-3">
                                         <div class="col-md-12">
@@ -25,16 +26,16 @@
                                                 Name <span class="text-danger">*</span>
                                             </label>
                                             <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                                id="name" name="name" placeholder="Enter the department name"
-                                                value="{{ old('name') }}" required>
+                                                id="name" name="name" placeholder="Enter the ministry name"
+                                                value="{{ old('name', $ministry->name) }}" required>
                                             @error('name')
                                                 <div class="invalid-feedback"> {{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
                                 </div>
-                                <x-buttons.form-action primaryTitle="Create Departments" primaryId="createDepartmentBtn"
-                                    :cancel-route="route('staff.departments.index')" />
+                                <x-buttons.form-action primaryTitle="Edit Ministry" primaryId="createMinistryBtn"
+                                    :cancel-route="route('staff.ministries.index')" />
                             </form>
                         </div>
                     </div>
